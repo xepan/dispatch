1f7539a642260f9e48cce53a140afb6ac09f082b
sinharakesh <rksinha.btech@gmail.com>
2016-03-26 18:09:30 +0530

    dispatch initiator created

---
+++ lib/Initiator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace xepan\dispatch;
+
+class Initiator extends \Controller_Addon {
+	public $addon_name = 'xepan_dispatch';
+
+	function init(){
+		parent::init();
+
+		
+		$this->routePages('xepan_dispatch');
+		$this->addLocation(array('template'=>'templates','js'=>'templates/js'))
+			->setBaseURL('../vendor/xepan/dispatch/');
+
+		if($this->app->is_admin){
+			$this->app->side_menu->addItem('Dispatch','jkj');
+		}
+		
+	}
+}
